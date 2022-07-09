32f69bde262745083b44330355f3ef8fa35812ec
Marek <marek.wojciechowicz25@gmail.com>
2022-07-09 16:53:44 +0200

    Older class component with constructor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Hotels from './components/Hotels/Hotels';
 
-function App() {
-	return (
-		<div className="App">
-			<Header />
-      <Menu />
-      <Hotels />
-		</div>
-	);
+class App extends Component {
+constructor(props){
+	super(props);
+	this.state = {
+		hotels: [
+			{
+				id: 1,
+				name: 'Pod akacjami',
+				city: 'Białystok',
+				rating: '8.3',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+				image: ''
+			},
+			{
+				id: 2,
+				name: 'Poniatowski',
+				city: 'Suchowola',
+				rating: '8.2',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+				image: ''
+			},
+		]
+	}
+}
+
+
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<Menu />
+				<Hotels />
+			</div>
+		);
+	}
 }
 
 export default App;
