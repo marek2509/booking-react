bd2d138467ca8c3c5af10e7737b68fca1ec97879
Marek <marek.wojciechowicz25@gmail.com>
2022-07-09 16:44:06 +0200

    Get value after click Enter. (onKeyDown)

--- src/components/Header/Searchbar/Searchbar.js
+++ src/components/Header/Searchbar/Searchbar.js
@@ -2,23 +2,32 @@ import React, { useState } from 'react';
 import styles from './Searchbar.module.css';
 
 function Searchbar() {
-	
 	const [term, setTerm] = useState();
-	
+
 	const search = () => {
 		console.log('Szukaj', term);
+	};
+
+	const onKeyDownHandler = (e) => {
+		if (e.key === 'Enter') {
+			search();
+		}
 	};
 
 	return (
 		<div className="d-flex">
 			<input
 				value={term}
+				onKeyDown={onKeyDownHandler}
 				onChange={(e) => setTerm(e.target.value)}
 				className="form-control"
 				type="text"
 				placeholder="Szukaj..."
 			/>
-			<button onClick={search} className={`${styles.btnSearch} btn btn-secondary`}>
+			<button
+				onClick={search}
+				className={`${styles.btnSearch} btn btn-secondary`}
+			>
 				Szukaj!
 			</button>
 		</div>
