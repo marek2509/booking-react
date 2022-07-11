a1a34501ef41fa223d432a8ac5705afbce5b4274
Marek <marek.wojciechowicz25@gmail.com>
2022-07-11 20:19:54 +0200

    Added paralax - Hoc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,23 @@ import styles from './Header.module.css';
 import withMousePosition from '../../hoc/withMousePosition';
 
 function Header(props) {
+	const paralaxStyles = {
+		transform: `translate(
+			${props.mouseX / -50}px,
+			${props.mouseY / -120}px
+		)
+		`
+	} 
+
 	return (
-		<header className={`${styles.header}`}>
-			<div className='text-primary'>
-			{props.mouseX}
-			<br />
-			{props.mouseY}
+		<header className={`${styles.header}`}
+		
+		>
+			<div className={styles.headerImage} style={paralaxStyles}>
 			</div>
+
 			{props.children}
+		
 		</header>
 	);
 }
