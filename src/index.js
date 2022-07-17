14e1d6f951ddd20d7d994fcd5e7a6e77d0573355
Marek <marek.wojciechowicz25@gmail.com>
2022-07-17 19:49:23 +0200

    My own useState()

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import  'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let state = [];
+let index = 0;
+
+function useState(defaultvalue) {
+	const id = index++;
+
+	if (state[id]) {
+		return state[id];
+	}
+
+	const setValue = (newValue) => {
+		state[id][0] = newValue;
+		render();
+	};
+
+	const currentState = [defaultvalue, setValue];
+	state[id] = currentState;
+	return currentState;
+}
+
+function TestHook() {
+	const [value, setValue] = useState('start');
+	const [text, setText] = useState('frugi stan');
+
+	return (
+		<>
+			<h1>Test hook!</h1>
+			{text}
+			<input
+				type="text"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+		</>
+	);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function render() {
+  index = 0;
+	root.render(
+		<React.StrictMode>
+			<TestHook />
+			{/* <App /> */}
+		</React.StrictMode>
+	);
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
