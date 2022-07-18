e21f15f4fa23660febe7d1c5c71f7099951c7415
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2022-07-18 11:00:14 +0200

    useDebugValue() - info in devtools

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,47 +1,43 @@
-import React from 'react';
-import styles from './Menu.module.css';
-import AuthContext from '../../context/authContext';
-import useAuth from '../../hooks/useAuth';
+import React from "react";
+import styles from "./Menu.module.css";
+import useAuth from "../../hooks/useAuth";
 
 function Menu() {
+  const [auth, setAuth] = useAuth();
 
-	const [auth, setAuth] = useAuth();
-console.log("auth", auth);
-	const login = (e) => {
-		e.preventDefault();
-		// auth.login();
-		setAuth(true);
-	};
+  const login = (e) => {
+    e.preventDefault();
+    // login
+    setAuth(true);
+  };
 
-	const logout = (e) => {
-		e.preventDefault();
-		// auth.logout();
-		setAuth(false);
-	};
+  const logout = (e) => {
+    e.preventDefault();
+    // logout
+    setAuth(false);
+  };
 
-	return (
-		<div className={`${styles.menuContainer}`}>
-			<ul className={styles.menu}>
-				<li className={styles.menuItem}>
-					<a href="#">Home</a>
-					
-				</li>
+  return (
+    <div className={`${styles.menuContainer}`}>
+      <ul className={styles.menu}>
+        <li className={styles.menuItem}>
+          <a href="#">Home</a>
+        </li>
 
-				<li className={styles.menuItem}>
-					{auth ? (
-						<a href="#" onClick={logout}>
-							Wyloguj
-						</a>
-					) : (
-						<a href="#" onClick={login}>
-							Zaloguj
-						</a>
-					)}
-				</li>
-				
-			</ul>
-		</div>
-	);
+        <li className={styles.menuItem}>
+          {auth ? (
+            <a href="#" onClick={logout}>
+              Wyloguj
+            </a>
+          ) : (
+            <a href="#" onClick={login}>
+              Zaloguj
+            </a>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Menu;
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,10 +1,14 @@
-import { useContext } from "react";
+import { useContext, useDebugValue } from "react";
 import AuthContext from "../context/authContext";
 
 export default function useAuth() {
   const authContext = useContext(AuthContext);
 
   const auth = authContext.isAuthenticated;
+
+  // zmienia informacje pokazywaną w dev-tool
+  useDebugValue(auth ? "Zalogowany" : "Wylogowany");
+
   const setAuth = (value) => {
     if (value) {
       authContext.login();
