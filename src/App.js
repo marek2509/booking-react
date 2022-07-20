8bcdd27ac1a28971a0d6454572bf3c90b0ea648c
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2022-07-20 12:00:47 +0200

    Added 404 not found

--- src/App.js
+++ src/App.js
@@ -1,79 +1,87 @@
-import React, { useReducer } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
-import Menu from './components/Menu/Menu';
-import Searchbar from './components/UI/Searchbar/Searchbar';
-import Layout from './components/Layout/Layout';
-import Footer from './components/Footer/Footer';
-import ThemeButton from './components/UI/ThemeButton/ThemeButton';
-import ThemeContext from './context/themeContext';
-import AuthContext from './context/authContext';
-import ReducerContext from './context/reducerContext';
-import InspiringQuote from './components/InspiringQuote/InspiringQuote';
-import { reducer, initialState } from './reducer';
-import Home from './pages/Home/Home';
-import Hotel from './pages/Hotel/Hotel';
-import Search from './pages/Search/Search';
-import Profile from './pages/Profile/Profile';
+import React, { useReducer } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
+import Searchbar from "./components/UI/Searchbar/Searchbar";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
+import ThemeButton from "./components/UI/ThemeButton/ThemeButton";
+import ThemeContext from "./context/themeContext";
+import AuthContext from "./context/authContext";
+import ReducerContext from "./context/reducerContext";
+import InspiringQuote from "./components/InspiringQuote/InspiringQuote";
+import { reducer, initialState } from "./reducer";
+import Home from "./pages/Home/Home";
+import Hotel from "./pages/Hotel/Hotel";
+import Search from "./pages/Search/Search";
+import Profile from "./pages/Profile/Profile";
+import NotFound from "./pages/404/404";
 
 function App() {
-	const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-	const header = (
-		<Header>
-			<InspiringQuote />
-			<Searchbar />
-			<ThemeButton />
-		</Header>
-	);
+  const header = (
+    <Header>
+      <InspiringQuote />
+      <Searchbar />
+      <ThemeButton />
+    </Header>
+  );
 
-	const menu = <Menu />;
+  const menu = <Menu />;
 
   const content = (
     <>
       <Routes>
         <Route path="/hotele/:id" element={<Hotel />} />
-        <Route path="/wyszukaj/:term" element={<Search />} />
+        {/* <Route path="/wyszukaj/:term" element={<Search />} /> */}
+
+        <Route path="/wyszukaj" element={<Search />}>
+          <Route path=":term" element={<Search />} />
+          <Route path="" element={<Search />} />
+        </Route>
+
         <Route path="/profil/*" exact element={<Profile />} />
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
-	const footer = <Footer />;
+  const footer = <Footer />;
 
-	return (
-		<Router>
-			<AuthContext.Provider
-				value={{
-					isAuthenticated: state.isAuthenticated,
-					login: () => dispatch({ type: 'login' }),
-					logout: () => dispatch({ type: 'logout' }),
-				}}
-			>
-				<ThemeContext.Provider
-					value={{
-						color: state.theme,
-						onChangeTheme: () => dispatch({ type: 'change-theme' }),
-					}}
-				>
-					<ReducerContext.Provider
-						value={{
-							state: state,
-							dispatch: dispatch,
-						}}
-					>
-						<Layout
-							header={header}
-							menu={menu}
-							content={content}
-							footer={footer}
-						/>
-					</ReducerContext.Provider>
-				</ThemeContext.Provider>
-			</AuthContext.Provider>
-		</Router>
-	);
+  return (
+    <Router>
+      <AuthContext.Provider
+        value={{
+          isAuthenticated: state.isAuthenticated,
+          login: () => dispatch({ type: "login" }),
+          logout: () => dispatch({ type: "logout" }),
+        }}
+      >
+        <ThemeContext.Provider
+          value={{
+            color: state.theme,
+            onChangeTheme: () => dispatch({ type: "change-theme" }),
+          }}
+        >
+          <ReducerContext.Provider
+            value={{
+              state: state,
+              dispatch: dispatch,
+            }}
+          >
+            <Layout
+              header={header}
+              menu={menu}
+              content={content}
+              footer={footer}
+            />
+          </ReducerContext.Provider>
+        </ThemeContext.Provider>
+      </AuthContext.Provider>
+    </Router>
+  );
 }
 
 export default App;
