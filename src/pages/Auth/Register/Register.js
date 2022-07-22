bc0bcd8b3711006980d8bc645d492eae614d6142
Marek <marek.wojciechowicz25@gmail.com>
2022-07-22 19:48:59 +0200

    Fetch vs Axios

--- src/pages/Auth/Register/Register.js
+++ src/pages/Auth/Register/Register.js
@@ -1,93 +1,116 @@
-import React, { useEffect, useState } from "react";
-import LoadingButton from "../../../components/UI/LoadingButton/LoadingButton";
-import { validate } from "../../../helpers/validations";
-import Input from "../../../components/Input/Input";
+import React, { useEffect, useState } from 'react';
+import LoadingButton from '../../../components/UI/LoadingButton/LoadingButton';
+import { validate } from '../../../helpers/validations';
+import Input from '../../../components/Input/Input';
+import axios from 'axios';
 
 export default function Register(props) {
-  const [loading, setLoading] = useState(false);
-  const [form, setForm] = useState({
-    email: {
-      value: "",
-      error: "Pole wymagane",
-      showError: false,
-      rules: ["required", "email"],
-    },
-    password: {
-      value: "",
-      error: "Pole wymagane",
-      showError: false,
-      rules: ["required"],
-    },
-  });
+	const [loading, setLoading] = useState(false);
+	const [form, setForm] = useState({
+		email: {
+			value: '',
+			error: 'Pole wymagane',
+			showError: false,
+			rules: ['required', 'email'],
+		},
+		password: {
+			value: '',
+			error: 'Pole wymagane',
+			showError: false,
+			rules: ['required'],
+		},
+	});
 
-  const [Valid1, setValid] = useState(false);
+	const [Valid1, setValid] = useState(false);
 
-  //   let valid = false;
-  //   valid = Object.values(form).every((x) => x.error == false);
+	//   let valid = false;
+	//   valid = Object.values(form).every((x) => x.error == false);
 
-  const submit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  };
+	const submit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
 
-  const changeHandler = (value, fieldName) => {
-    const error = validate(form[fieldName].rules, value);
-    setForm({
-      ...form,
-      [fieldName]: {
-        ...form[fieldName],
-        value: value,
-        showError: true,
-        error: error,
-      },
-    });
-  };
+		const resAxios = await axios.get(
+			'https://react-hotels-6b5b6-default-rtdb.europe-west1.firebasedatabase.app/users.json'
+		);
 
-  useEffect(() => {
-    console.log(Object.values(form).every((x) => x.error == ""));
-    setValid(Object.values(form).every((x) => x.error == ""));
-  }, [form]);
+		console.log(resAxios);
+		const response = await fetch(
+			'https://react-hotels-6b5b6-default-rtdb.europe-west1.firebasedatabase.app/users.json',
+			{
+				// method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				// body: JSON.stringify({
+				// 	email: 'test@email.com',
+				// 	password: 'tajneHaslo',
+				// }),
+			}
+		);
 
+		const content = await response.json();
+		console.log(content);
+		setTimeout(() => {
+			setLoading(false);
+		}, 500);
+	};
 
-  return (
-    <div className="card">
-      <div className="card-header">Rejestracja</div>
-      <div className="card-body">
-        <p className="text-muted">Uzupełnij dane</p>
+	const changeHandler = (value, fieldName) => {
+		const error = validate(form[fieldName].rules, value);
+		setForm({
+			...form,
+			[fieldName]: {
+				...form[fieldName],
+				value: value,
+				showError: true,
+				error: error,
+			},
+		});
+	};
 
-        <form onSubmit={submit}>
-          <Input
-            label="Email"
-            type="email"
-            value={form.email.value}
-            onChange={(val) => changeHandler(val, "email")}
-            error={form.email.error}
-            showError={form.email.showError}
-          />
+	useEffect(() => {
+		console.log(Object.values(form).every((x) => x.error == ''));
+		setValid(Object.values(form).every((x) => x.error == ''));
+	}, [form]);
 
-          <Input
-            label="Hasło"
-            type="password"
-            value={form.password.value}
-            onChange={(val) => changeHandler(val, "password")}
-            error={form.password.error}
-            showError={form.password.showError}
-          />
+	return (
+		<div className="card">
+			<div className="card-header">Rejestracja</div>
+			<div className="card-body">
+				<p className="text-muted">Uzupełnij dane</p>
 
-          <div className="text-right">
-            <LoadingButton
-              disabled={!Valid1}
-              loading={loading}
-              className="btn-success"
-            >
-              Zarejestruj
-            </LoadingButton>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+				<form onSubmit={submit}>
+					<Input
+						label="Email"
+						type="email"
+						value={form.email.value}
+						onChange={(val) => changeHandler(val, 'email')}
+						error={form.email.error}
+						showError={form.email.showError}
+					/>
+
+					<Input
+						label="Hasło"
+						type="password"
+						value={form.password.value}
+						onChange={(val) => changeHandler(val, 'password')}
+						error={form.password.error}
+						showError={form.password.showError}
+					/>
+
+					<div className="text-right">
+						<LoadingButton
+							disabled={!Valid1}
+							loading={loading}
+							className="btn-success"
+						>
+							Zarejestruj
+						</LoadingButton>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
