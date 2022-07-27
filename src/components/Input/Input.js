d6af87e66b7f2c2e2db0b4cb12d5cfd5cd32070b
Marek <marek.wojciechowicz25@gmail.com>
2022-07-27 20:13:05 +0200

    Adding hotels to the database

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,169 +1,167 @@
-import React, { useRef } from "react";
-import PropTypes from "prop-types";
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
 
 const InputText = (props) => {
-  return (
-    <div className="form-group">
-      <label>{props.label}</label>
-      <input
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
-        type={props.type}
-        className={`form-control ${
-          props.error && props.showError ? "is-invalid" : ""
-        }`}
-      />
-      <div className="invalid-feedback">{props.error}</div>
-    </div>
-  );
+	return (
+		<div className="form-group">
+			<label>{props.label}</label>
+			<input
+				value={props.value}
+				onChange={(e) => props.onChange(e.target.value)}
+				type={props.type}
+				className={`form-control ${
+					props.error && props.showError ? 'is-invalid' : ''
+				}`}
+			/>
+			<div className="invalid-feedback">{props.error}</div>
+		</div>
+	);
 };
 
 const InputTextarea = (props) => {
-  return (
-    <div className="form-group">
-      <label>{props.label}</label>
-      <textarea
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
-        type={props.type}
-        className={`form-control ${
-          props.error && props.showError ? "is-invalid" : ""
-        }`}
-      />
-      <div className="invalid-feedback">{props.error}</div>
-    </div>
-  );
+	return (
+		<div className="form-group">
+			<label>{props.label}</label>
+			<textarea
+				value={props.value}
+				onChange={(e) => props.onChange(e.target.value)}
+				type={props.type}
+				className={`form-control ${
+					props.error && props.showError ? 'is-invalid' : ''
+				}`}
+			/>
+			<div className="invalid-feedback">{props.error}</div>
+		</div>
+	);
 };
 
 const InputSelect = (props) => {
-  return (
-    <div className="form-group">
-      <label>{props.label}</label>
-      <select
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
-        className={`form-control ${false ? "is-invalid" : ""}`}
-      >
-        {props.options.map((option) => {
-          return (
-            <option value={option.value} key={option.value}>
-              {option.label}
-            </option>
-          );
-        })}
-      </select>
-      <div className="invalid-feedback">Błąd</div>
-    </div>
-  );
+	return (
+		<div className="form-group">
+			<label>{props.label}</label>
+			<select
+				value={props.value}
+				onChange={(e) => props.onChange(e.target.value)}
+				className={`form-control ${false ? 'is-invalid' : ''}`}
+			>
+				{props.options.map((option) => {
+					return (
+						<option value={option.value} key={option.value}>
+							{option.label}
+						</option>
+					);
+				})}
+			</select>
+			<div className="invalid-feedback">Błąd</div>
+		</div>
+	);
 };
 
 const InputFile = (props) => {
-  const fileRef = useRef();
+	const fileRef = useRef();
 
-  const changeFileHandler = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      props.onChange(file);
-    }
-  };
+	const changeFileHandler = (e) => {
+		const file = e.target.files[0];
+		if (file) {
+			props.onChange(file);
+		}
+	};
 
-  return (
-    <div className="form-group">
-      <input type={props.type} onChange={changeFileHandler} />
-    </div>
-  );
+	return (
+		<div className="form-group">
+			<input type={props.type} onChange={changeFileHandler} />
+		</div>
+	);
 };
 
 const InputRadio = (props) => {
-  const changeHandler = (e) => {
-    const status = e.target.value;
-    props.onChange(status);
-  };
-  return (
-    <div className="form-group">
-      {props.options.map((option) => (
-        <div
-          className="custom-control custom-checkbox"
-          key={`radio-${option.value}-${props.name}`}
-        >
-          <input
-            className="custom-control-input"
-            type={props.type}
-            name={props.name}
-            value={option.value}
-            checked={option.value == props.value}
-            id={`radio-${option.value}-${props.name}`}
-            onChange={changeHandler}
-          />
-          <label
-            className="custom-control-label"
-            htmlFor={`radio-${option.value}-${props.name}`}
-          >
-            {option.label}
-          </label>
-        </div>
-      ))}
-    </div>
-  );
+	const changeHandler = (e) => {
+		const status = e.target.value;
+		props.onChange(status);
+	};
+	return (
+		<div className="form-group">
+			{props.options.map((option) => (
+				<div
+					className="custom-control custom-checkbox"
+					key={`radio-${option.value}-${props.name}`}
+				>
+					<input
+						className="custom-control-input"
+						type={props.type}
+						name={props.name}
+						value={option.value}
+						checked={option.value == props.value}
+						id={`radio-${option.value}-${props.name}`}
+						onChange={changeHandler}
+					/>
+					<label
+						className="custom-control-label"
+						htmlFor={`radio-${option.value}-${props.name}`}
+					>
+						{option.label}
+					</label>
+				</div>
+			))}
+		</div>
+	);
 };
 
 const InputCheckbox = (props) => {
-  const changeFeatureHandler = (e) => {
-    const value = e.target.value;
-    const isChecked = e.target.checked;
-    if (isChecked) {
-      const newValue = [...props.value, value];
-      props.onChange(newValue);
-    } else {
-      const newValue = props.value.filter((x) => x !== value);
-      props.onChange(newValue);
-    }
-  };
+	const changeFeatureHandler = (e) => {
+		const value = e.target.value;
+		const isChecked = e.target.checked;
+		if (isChecked) {
+			const newValue = [...props.value, value];
+			props.onChange(newValue);
+		} else {
+			const newValue = props.value.filter((x) => x !== value);
+			props.onChange(newValue);
+		}
+	};
 
-  return (
-    <div className="form-group">
-      {props.options.map((option) => (
-        <div className="custom-control custom-checkbox" key={option.value}>
-          <input
-            className="custom-control-input"
-            type="checkbox"
-            id={option.value}
-            value={option.value}
-            checked={props.value.find((x) => x === option.value)}
-            onChange={changeFeatureHandler}
-          />
-          <label className="custom-control-label" htmlFor={option.value}>
-            {option.label}
-          </label>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="form-group">
+			{props.options.map((option) => (
+				<div className="custom-control custom-checkbox" key={option.value}>
+					<input
+						className="custom-control-input"
+						type="checkbox"
+						id={option.value}
+						value={option.value}
+						checked={props.value.find((x) => x === option.value)}
+						onChange={changeFeatureHandler}
+					/>
+					<label className="custom-control-label" htmlFor={option.value}>
+						{option.label}
+					</label>
+				</div>
+			))}
+		</div>
+	);
 };
 
-
-
 function Input(props) {
-  switch (props.type) {
-    case "select":
-      return <InputSelect {...props} />;
-    case "checkbox":
-      return <InputCheckbox {...props} />;
-    case "file":
-      return <InputFile {...props} />;
-    case "radio":
-      return <InputRadio {...props} />;
-    case "textarea":
-      return <InputTextarea {...props} />;
-    default:
-      return <InputText {...props} />;
-  }
+	switch (props.type) {
+		case 'select':
+			return <InputSelect {...props} />;
+		case 'checkbox':
+			return <InputCheckbox {...props} />;
+		case 'file':
+			return <InputFile {...props} />;
+		case 'radio':
+			return <InputRadio {...props} />;
+		case 'textarea':
+			return <InputTextarea {...props} />;
+		default:
+			return <InputText {...props} />;
+	}
 }
 
 Input.defaultProps = {
-  type: "text",
-  isValid: false,
-  showError: false,
+	type: 'text',
+	isValid: false,
+	showError: false,
 };
 
 export default Input;
--- src/pages/Profile/MyHotels/AddHotel/AddHotel.js
+++ src/pages/Profile/MyHotels/AddHotel/AddHotel.js
@@ -1,174 +1,193 @@
-import React, { useRef, useState } from "react";
-import LoadingButton from "../../../../components/UI/LoadingButton/LoadingButton";
-import Input from "../../../../components/Input/Input";
-import { validate } from "../../../../helpers/validations";
+import React, { useRef, useState } from 'react';
+import LoadingButton from '../../../../components/UI/LoadingButton/LoadingButton';
+import Input from '../../../../components/Input/Input';
+import { validate } from '../../../../helpers/validations';
+import axios from '../../../../axios';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../../../hooks/useAuth';
 
 const AddHotel = (props) => {
-  //   const [form, setForm] = useState({
-  //     name: "",
-  //     description: "",
-  //     city: "",
-  //     rooms: "",
-  //     features: [],
-  //     image: null,
-  //     status: 0,
-  //   });
+	//   const [form, setForm] = useState({
+	//     name: "",
+	//     description: "",
+	//     city: "",
+	//     rooms: "",
+	//     features: [],
+	//     image: null,
+	//     status: 0,
+	//   });
+const [auth] = useAuth();
+	const [form, setForm] = useState({
+		name: {
+			value: '',
+			error: '',
+			showError: false,
+			rules: ['required', { rule: 'min', length: 4 }],
+		},
+		description: {
+			value: '',
+			error: '',
+			showError: false,
+			rules: ['required', { rule: 'min', length: 10 }],
+		},
+		city: {
+			value: '',
+			error: '',
+			showError: false,
+			rules: ['required'],
+		},
+		rooms: {
+			value: '2',
+			error: '',
+			showError: false,
+			rules: ['required'],
+		},
+		features: {
+			value: [],
+			error: '',
+			showError: false,
+			rules: [],
+		},
+		image: {
+			value: null,
+			error: '',
+			showError: false,
+			rules: [],
+		},
+		status: {
+			value: 0,
+			error: '',
+			showError: false,
+			rules: ['required'],
+		},
+	});
 
-  const [form, setForm] = useState({
-    name: {
-      value: "",
-      error: "",
-      showError: false,
-      rules: ["required", { rule: "min", length: 4 }],
-    },
-    description: {
-      value: "",
-      error: "",
-      showError: false,
-      rules: ["required" ,{ rule: "min", length: 10 }],
-    },
-    city: {
-      value: "",
-      error: "",
-      showError: false,
-      rules: ["required"],
-    },
-    rooms: {
-      value: "2",
-      error: "",
-      showError: false,
-      rules: ["required"],
-    },
-    features: {
-      value: [],
-      error: "",
-      showError: false,
-      rules: [],
-    },
-    image: {
-      value: null,
-      error: "",
-      showError: false,
-      rules: [],
-    },
-    status: {
-      value: "",
-      error: "",
-      showError: false,
-      rules: ["required"],
-    },
-  });
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 
-  const [loading, setLoading] = useState(false);
+	const submit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+		try {
+			const dataHotel = {
+				name: form.name.value,
+				description: form.description.value,
+				city: form.city.value,
+				rooms: form.rooms.value,
+				features: form.features.value,
+				status: form.status.value,
+				user_id: auth.userId,
+			};
 
-  const submit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  };
+			await axios.post('/hotels.json', dataHotel);
+			navigate('/profil/hotele');
+		} catch (ex) {
+			console.log(ex.response);
+		}
+		setLoading(false);
+	};
 
-  const changeHandler = (value, fieldName) => {
-    const error = validate(form[fieldName].rules, value);
-    setForm({
-      ...form,
-      [fieldName]: {
-        ...form[fieldName],
-        value: value,
-        showError: true,
-        error: error,
-      },
-    });
-  };
+	const changeHandler = (value, fieldName) => {
+    console.log(value, fieldName)
+		const error = validate(form[fieldName].rules, value);
+		setForm({
+			...form,
+			[fieldName]: {
+				...form[fieldName],
+				value: value,
+				showError: true,
+				error: error,
+			},
+		});
+	};
 
-  return (
-    <div className="card">
-      <div className="card-header">Nowy hotel</div>
-      <div className="card-body">
-        <p className="text-muted">Uzupełnij dane hotelu</p>
+	return (
+		<div className="card">
+			<div className="card-header">Nowy hotel</div>
+			<div className="card-body">
+				<p className="text-muted">Uzupełnij dane hotelu</p>
 
-        <form onSubmit={submit}>
-          <Input
-            label="Nazwa"
-            value={form.name.value}
-            onChange={(val) => changeHandler(val, "name")}
-            error={form.name.error}
-            showError={form.name.showError}
-          />
+				<form onSubmit={submit}>
+					<Input
+						label="Nazwa"
+						value={form.name.value}
+						onChange={(val) => changeHandler(val, 'name')}
+						error={form.name.error}
+						showError={form.name.showError}
+					/>
 
-          <Input
-            label="Opis"
-            type="textarea"
-            value={form.description.value}
-            onChange={(val) => changeHandler(val, "description")}
-            error={form.description.error}
-            showError={form.description.showError}
-          />
+					<Input
+						label="Opis"
+						type="textarea"
+						value={form.description.value}
+						onChange={(val) => changeHandler(val, 'description')}
+						error={form.description.error}
+						showError={form.description.showError}
+					/>
 
-          <Input
-            label="Miejscowość"
-            value={form.city.value}
-            onChange={(val) => changeHandler(val, "city")}
-            error={form.city.error}
-            showError={form.city.showError}
-          />
+					<Input
+						label="Miejscowość"
+						value={form.city.value}
+						onChange={(val) => changeHandler(val, 'city')}
+						error={form.city.error}
+						showError={form.city.showError}
+					/>
 
-          <Input
-            label="Ilość pokoi"
-            value={form.rooms.value}
-            type="select"
-            onChange={(val) => changeHandler(val, "rooms")}
-            options={[
-              { value: 1, label: 1 },
-              { value: 2, label: 2 },
-              { value: 3, label: 3 },
-              { value: 4, label: 4 },
-            ]}
-            error={form.rooms.error}
-            showError={form.rooms.showError}
-          />
+					<Input
+						label="Ilość pokoi"
+						value={form.rooms.value}
+						type="select"
+						onChange={(val) => changeHandler(val, 'rooms')}
+						options={[
+							{ value: 1, label: 1 },
+							{ value: 2, label: 2 },
+							{ value: 3, label: 3 },
+							{ value: 4, label: 4 },
+						]}
+						error={form.rooms.error}
+						showError={form.rooms.showError}
+					/>
 
-          <h4 className="mt-3">Udogodnienia</h4>
+					<h4 className="mt-3">Udogodnienia</h4>
 
-          <Input
-            type="checkbox"
-            value={form.features.value}
-            onChange={(val) => changeHandler(val, "features")}
-            options={[
-              { value: "tv", label: "TV" },
-              { value: "wifi", label: "Wi-fi" },
-              { value: "parking", label: "Parking" },
-            ]}
-            error={form.features.error}
-            showError={form.features.showError}
-          />
+					<Input
+						type="checkbox"
+						value={form.features.value}
+						onChange={(val) => changeHandler(val, 'features')}
+						options={[
+							{ value: 'tv', label: 'TV' },
+							{ value: 'wifi', label: 'Wi-fi' },
+							{ value: 'parking', label: 'Parking' },
+						]}
+						error={form.features.error}
+						showError={form.features.showError}
+					/>
 
-          <h4 className="mt-3">Zdjęcie</h4>
-          <Input type="file" onChange={(val) => changeHandler(val, "image")} />
+					<h4 className="mt-3">Zdjęcie</h4>
+					<Input type="file" onChange={(val) => changeHandler(val, 'image')} />
 
-          <h4 className="mt-3">Aktywny</h4>
+					<h4 className="mt-3">Aktywny</h4>
 
-          <Input
-            type="radio"
-            value={form.status.value}
-            onChange={(val) => changeHandler(val, "status")}
-            name="status"
-            options={[
-              { label: "Aktywny", value: 1 },
-              { label: "Ukryty", value: 0 },
-            ]}
-          />
+					<Input
+						type="radio"
+						value={form.status.value}
+						onChange={(val) => changeHandler(val, 'status')}
+						name="status"
+						options={[
+							{ label: 'Aktywny', value: 1 },
+							{ label: 'Ukryty', value: 0 },
+						]}
+					/>
 
-          <div className="text-right">
-            <LoadingButton loading={loading} className="btn-success">
-              Dodaj hotel
-            </LoadingButton>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+					<div className="text-right">
+						<LoadingButton loading={loading} className="btn-success">
+							Dodaj hotel
+						</LoadingButton>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default AddHotel;
